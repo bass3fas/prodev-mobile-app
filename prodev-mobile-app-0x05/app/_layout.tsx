029a8a0d259ac97e2400a148cfeddfde6b172ce1
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2025-02-24 11:40:37 +0400

    task2 step4

--- prodev-mobile-app-0x05/app/_layout.tsx
+++ prodev-mobile-app-0x05/app/_layout.tsx
@@ -8,6 +8,7 @@ export default function RootLayout() {
       <Stack.Screen name="index" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
+      <Stack.Screen name="(home)" />
     </Stack>
   );
 }
