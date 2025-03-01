5f7fb1169c0d432c0900afe9849dfa31ac3415e3
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2025-03-01 10:18:02 +0400

    try

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -28,7 +28,8 @@ const HomeRootLayout = () => {
       }} />
       <Tabs.Screen name="profile" options={{
         title: 'Profile',
-        
+        headerShown: false,
+        tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color={color} />
       }} />
     </Tabs>
   )
